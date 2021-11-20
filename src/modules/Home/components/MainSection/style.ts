5d83a865a122ccd00e7fa93576e207fd74532d18
Gabriel Manfroi <gabsmanfroi@gmail.com>
2021-11-20 18:09:45 -0300

    💄 Adds Main Section Component

---
+++ src/modules/Home/components/MainSection/style.ts
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const MainSectionContainer = styled.main`
+  height: 100vh;
+  background: url('assets/images/Barbershop04.jpg');
+  background-size: cover;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  padding: 10%;
+  color: white;
+
+  &::before {
+    z-index: 1;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    content: '';
+    background: #000000ac;
+  }
+`;
+
+export const MainSectionContent = styled.div`
+  z-index: 2;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+
+  .up-content {
+    font-size: 60px;
+    text-transform: uppercase;
+  }
+
+  .middle-content {
+    align-items: center;
+    .slogan {
+      font-size: 45px;
+    }
+  }
+
+  .down-content {
+    .button-group {
+      display: flex;
+      justify-content: center;
+      button {
+        margin: 0 10px;
+      }
+    }
+  }
+`;
