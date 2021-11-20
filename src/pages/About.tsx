c9a4c3fa189419bbf384ab051d45381b65ed81af
Gabriel Manfroi <gabsmanfroi@gmail.com>
2021-11-20 18:08:38 -0300

    💄 Initial Pages Implementation

---
+++ src/pages/About.tsx
@@ -0,0 +1,5 @@
+const AboutPage = () => {
+  return <h1>About Works!</h1>;
+};
+
+export default AboutPage;
---
+++ src/pages/Schedule.tsx
@@ -0,0 +1,5 @@
+const SchedulePage = () => {
+  return <h1>Schedule Works!</h1>;
+};
+
+export default SchedulePage;
