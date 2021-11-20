1a55cf571028f20e24b3eccc763086141ef32e63
Gabriel Manfroi <gabsmanfroi@gmail.com>
2021-11-20 18:10:11 -0300

    💄  Theming and Routing

---
+++ src/modules/Shared/util/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from '@material-ui/core';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#ffff8b',
+      main: '#ffde59',
+      dark: '#c9ad24',
+      contrastText: '#000000',
+    },
+  },
+});
