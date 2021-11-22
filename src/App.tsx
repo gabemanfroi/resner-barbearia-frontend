c0b6da135555dc4605b031f25e9e912677e5c936
Gabriel Manfroi <gabsmanfroi@gmail.com>
2021-11-21 23:21:50 -0300

    🌐 Adds Localization Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
 import { About, Home, Portfolio, Schedule } from 'pages';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from 'modules/Shared/components';
 import { ThemeProvider } from '@material-ui/core';
 import { theme } from 'modules/Shared/util/theme';
+import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
+import LocalizationProvider from '@material-ui/lab/LocalizationProvider';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sobre" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/agendamento" element={<Schedule />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <LocalizationProvider dateAdapter={AdapterDateFns}>
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre" element={<About />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/agendamento" element={<Schedule />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </LocalizationProvider>
   );
 }
 
